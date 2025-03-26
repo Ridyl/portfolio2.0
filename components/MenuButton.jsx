a6a0258acd47d5c45f592224984fb70380138ccc
Ridyl <joey.dalatri214@gmail.com>
2025-03-26 17:08:25 -0400

    Fixed link issues

--- components/MenuButton.jsx
+++ components/MenuButton.jsx
@@ -201,7 +201,7 @@ export function MenuButton() {
 												</motion.li>
 												<motion.li variants={itemsVariants} key={'emailList'}>
 													<Link
-														href='/email'
+														href={'/contact'}
 														onClick={toggleOpen}
 														key={'emailItem'}
 													>

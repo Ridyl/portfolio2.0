68c86c05243aa19517d3ffb0ec8c95b1bb865fef
Ridyl <joey.dalatri214@gmail.com>
2025-04-03 21:51:13 -0400

    Contact form complete. Toast properly displays upon success or failure of message send

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -4,6 +4,7 @@ import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { toast } from 'sonner';
+import { Toaster } from '@/components/ui/sonner';
 
 import {
 	Form,
@@ -46,78 +47,89 @@ export function ContactForm() {
 
 		emailjs.send(serviceID, templateID, values, userID).then(
 			() => {
-				toast('Message sent successfully!');
+				toast.success('Message sent successfully!');
+				form.reset();
 			},
 			(error) => {
-				toast('Failed to send message. Please try again later.');
+				toast.error('Failed to send message. Please try again later.');
 			}
 		);
 	};
 
 	return (
-		<Form {...form}>
-			<form
-				onSubmit={form.handleSubmit(onSubmit)}
-				className='space-y-6 max-w-lg mx-auto p-4 border rounded-2xl border-sky-300 shadow-md'
-			>
-				<FormField
-					control={form.control}
-					name='firstName'
-					render={({ field }) => (
-						<FormItem>
-							<FormLabel>First Name</FormLabel>
-							<FormControl>
-								<Input placeholder='John' {...field} />
-							</FormControl>
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
+		<>
+			<Form {...form}>
+				<form
+					onSubmit={form.handleSubmit(onSubmit)}
+					className='space-y-6 max-w-lg mx-auto p-4 border rounded-2xl border-sky-300 shadow-md'
+				>
+					<FormField
+						control={form.control}
+						name='firstName'
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>First Name</FormLabel>
+								<FormControl>
+									<Input placeholder='John' {...field} />
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
 
-				<FormField
-					control={form.control}
-					name='lastName'
-					render={({ field }) => (
-						<FormItem>
-							<FormLabel>Last Name</FormLabel>
-							<FormControl>
-								<Input placeholder='Doe' {...field} />
-							</FormControl>
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
+					<FormField
+						control={form.control}
+						name='lastName'
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Last Name</FormLabel>
+								<FormControl>
+									<Input placeholder='Doe' {...field} />
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
 
-				<FormField
-					control={form.control}
-					name='email'
-					render={({ field }) => (
-						<FormItem>
-							<FormLabel>Email</FormLabel>
-							<FormControl>
-								<Input type='email' placeholder='john@example.com' {...field} />
-							</FormControl>
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
+					<FormField
+						control={form.control}
+						name='email'
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Email</FormLabel>
+								<FormControl>
+									<Input
+										type='email'
+										placeholder='john@example.com'
+										{...field}
+									/>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
 
-				<FormField
-					control={form.control}
-					name='message'
-					render={({ field }) => (
-						<FormItem>
-							<FormLabel>Message</FormLabel>
-							<FormControl>
-								<Textarea placeholder='Type your message here...' {...field} />
-							</FormControl>
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
+					<FormField
+						control={form.control}
+						name='message'
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Message</FormLabel>
+								<FormControl>
+									<Textarea
+										placeholder='Type your message here...'
+										{...field}
+									/>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
 
-				<Button type='submit'>Send Message</Button>
-			</form>
-		</Form>
+					<Button type='submit'>Send Message</Button>
+				</form>
+			</Form>
+			<Toaster richColors />
+		</>
 	);
 }
